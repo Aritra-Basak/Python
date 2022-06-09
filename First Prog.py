999ff51a1134bc2961d38b70977c356aa3a88880
Aritra Basak <basakaritra10@gmail.com>
2022-06-09 22:18:47 +0530

    Create First Prog.py

---
+++ First Prog.py
@@ -0,0 +1,10 @@
+print(10* 'Aritra')
+my="Aritra Basak"
+print(len(my))
+mix=['hi',23,45,60,79,83,'bye']
+print("Lenght of mix is", len(mix))
+print(mix[1])
+print(mix[2:])
+print(mix[0:3])
+print(mix[-1])
+print(mix[-7])
