b2d9f2db6671511d8c3a73026fecf46564bb821c
Aritra Basak <basakaritra10@gmail.com>
2022-06-24 22:35:38 +0530

    Create Complex_root.py

---
+++ Complex_root.py
@@ -0,0 +1,5 @@
+import cmath,math
+x = eval(input("Enter a normal number "))
+print ("Square Root of that number is ",math.sqrt(x))
+y = eval(input("Enter a complex number "))
+print ("Square Root of that complex number is ",cmath.sqrt(y))
