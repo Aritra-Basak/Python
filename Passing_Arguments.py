f0a9e5f26bf3f3bc5649b6f92391633fdc649c2b
Aritra Basak <basakaritra10@gmail.com>
2022-06-25 14:38:10 +0530

    Create Passing_Arguments.py

---
+++ Passing_Arguments.py
@@ -0,0 +1,6 @@
+#passing multiple arguments in python
+def PRINT(name,age):
+  print("my name is",name,"and my age is ",age)
+name=input("Enter ur name")
+age=int(input("Enter ur age"))
+PRINT(name,age)
