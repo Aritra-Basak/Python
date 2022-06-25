b84c53f9f880cd2637e97e302261114ed7b5a585
Aritra Basak <basakaritra10@gmail.com>
2022-06-25 14:26:41 +0530

    Create reversing_orientation.py

---
+++ reversing_orientation.py
@@ -0,0 +1,6 @@
+#reversing the orientation of a string without reversing the words.
+str1=input("Enter the sentence")
+st=[]
+st=str1.split(" ") #storing the string the list
+print(st)
+print(st[::-1])
