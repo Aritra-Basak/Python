4ec5e6636e5bb330b6afea5fa49030d87fedbe5a
Aritra Basak <basakaritra10@gmail.com>
2022-06-26 12:50:47 +0530

    Update Duplicate_word_remover.py

--- Duplicate_word_remover.py
+++ Duplicate_word_remover.py
@@ -4,8 +4,8 @@ l=[]
 l=a.split(" ")
 for i in range(0,len(l)):
   for j in range((i+1),len(l)):
-    if l[i]==l[j]:
+    if l[i]==l[j]:  #when bothe the words are same then the duplicate word is replaced by 0
       l[j]= 0
 for i in l:
-  if i!=0:
+  if i!=0:  # will print all the elements from the list other than 0
     print(i,end=" ")
